0a6c220981911da3d24c22f1c30b71c96c8c56bb
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-09-21 11:25:21 +0900

    Add logger.ts

---
+++ ts-node/src/logger.ts
@@ -0,0 +1,36 @@
+import { createLogger, format, transports, addColors } from 'winston';
+const colorizer = format.colorize();
+
+addColors({
+    debug: 'gray',
+    error: 'bold magenta',
+    info: 'green',
+    warn: 'yellow',
+});
+
+const logger = createLogger({
+    level: 'debug',
+    // format: format.json(),
+    format: format.combine(
+        format.timestamp({ format: 'HH:MM:SS' }),
+        format.printf(msg => colorizer.colorize(msg.level, `[${msg.timestamp}] <${msg.level.toUpperCase()}> ${msg.message}`)),
+    ),
+    defaultMeta: {
+        service: 'user-service'
+    },
+    transports: [
+        new transports.File({ filename: './log/error.log', level: 'error' }),
+        new transports.File({ filename: './log/combined.log' }),
+        // new transports.Console(),
+    ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new transports.Console());
+}
+
+// logger.info('this is info');
+// logger.error('this is error');
+// logger.warn('this is warn')
+
+export default logger;
